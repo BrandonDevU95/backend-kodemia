b9c878989015eaec318a0f119b82aa025002968c
BrandonDevKodemia <brandon.vargas1095@hotmail.com>
2024-05-17 12:07:59 -0600

    Practica 6

--- practica6/koders.js
+++ practica6/koders.js
@@ -10,52 +10,100 @@
 // existSync permite saber si un archivo existe o no
 // guardar en un archivo .json usando JSON.stringify
 const fs = require('fs');
+const db = 'db.json';
 
-const commad = process.argv[2];
-const name = process.argv[3];
-
-switch (commad) {
-	case 'add':
-		addKoder();
-		break;
-	case 'ls':
-		listKoder();
-		break;
-	case 'rm':
-		removeKoder();
-		break;
-	case 'reset':
-		removeAllKoders();
-		break;
-	default:
-		console.log('Command not found');
-		break;
-}
-
-function addKoder() {
-	console.log('Add koder');
+function addKoder(koder) {
+	const koders = getKoders();
+	koders.push(koder);
+	updateKoders(koders);
 }
 
 function listKoder() {
-	console.log('List koder');
+	const koders = getKoders();
+	if (!koders.length) {
+		console.log('No koders');
+		process.exit();
+	}
+	koders.forEach((koder, i) => {
+		console.log(i, '-', koder);
+	});
 }
 
-function removeKoder() {
-	console.log('Remove koder');
+function removeKoder(index) {
+	const koders = getKoders();
+	koders.splice(index, 1);
+	updateKoders(koders);
 }
 
 function removeAllKoders() {
-	console.log('Remove all koders');
+	updateKoders([]);
 }
 
 function getKoders() {
-	const content = fs.readFileSync(dbFile, 'utf8');
-	return JSON.parse(content).todos;
+	const content = fs.readFileSync(db, 'utf8');
+	return JSON.parse(content).koders;
 }
 
-function init() {
-	const db = 'db.json';
-	const fileExists = fs.existsSync(db);
-
-	if (!fileExists) fs.writeFileSync(db, JSON.stringify({}));
+function updateKoders(koders) {
+	const newkoders = { koders: koders };
+	const newkodersAsString = JSON.stringify(newkoders);
+	fs.writeFileSync(db, newkodersAsString);
 }
+
+function startDb() {
+	const dbExists = fs.existsSync(db);
+
+	if (!dbExists) fs.writeFileSync(db, JSON.stringify({ koders: [] }));
+}
+
+function main() {
+	const commad = process.argv[2];
+	const arg = process.argv[3];
+
+	startDb();
+
+	switch (commad) {
+		case 'add':
+			if (!arg) {
+				console.error('Missing koder');
+				process.exit(1);
+			}
+			addKoder(arg);
+			listKoder();
+			console.log('Koder added');
+			break;
+		case 'ls':
+			listKoder();
+			break;
+		case 'rm':
+			if (!arg) {
+				console.error('Missing koder index');
+				process.exit(1);
+			}
+			const idx = parseInt(arg);
+			if (isNaN(idx)) {
+				console.error('Invalid index');
+				process.exit(1);
+			}
+
+			const koders = getKoders();
+			if (idx < 0 || idx >= koders.lenght) {
+				console.error('Out range');
+				process.exit(1);
+			}
+			removeKoder(idx);
+			listKoder();
+			console.log('Koder deleted');
+			break;
+		case 'reset':
+			removeAllKoders();
+			console.log('All koders deleted');
+			break;
+		default:
+			console.error('Command not found', commad);
+			process.error(1);
+			break;
+	}
+}
+
+main();
